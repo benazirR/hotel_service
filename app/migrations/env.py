00a179271e55cb194f59745a63570491a2102280
ben <benazir.rajabov@gmail.com>
2024-11-11 22:42:44 +0500

    add new tables models

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -10,7 +10,9 @@ from alembic import context
 
 sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
 from app.database import Base, DATABASE_URL
-from app.hotels.models import Hotels
+from app.hotels.models import Hotels, Rooms
+from app.bookings.models import Bookings
+from app.users.models import User
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
