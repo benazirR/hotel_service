9fe15438436d71fff5febbd02fe7bf485918440f
B_R <b.rajabov@eskhata.tj>
2024-11-11 17:01:01 +0500

    Add simple get and post query

--- app/main.py
+++ app/main.py
@@ -1,17 +1,30 @@
-from fastapi import FastAPI, Query
+from dataclasses import Field
+
+from fastapi import FastAPI, Query, Depends
 from typing import Optional
 from datetime import date
 from pydantic import BaseModel
 
 app = FastAPI()
 
+class HotelsSearchArgs:
+    def __init__(self,
+                 location: str,
+                 date_from: date,
+                 date_to: date,
+                 stars: Optional[int] = Query(None, ge=1, le=5),
+                 has_spa: Optional[bool] = None
+                 ):
+        self.location = location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.stars = stars
+        self.has_spa = has_spa
+
+
 @app.get("/hotels")
-def get_hotels(location: str,
-               date_from: date,
-               date_to: date,
-               stars: Optional[int] = Query(None, ge=1, le=5),
-               has_spa: Optional[bool] = None,):
-    return location, date_from, date_to, stars, has_spa
+def get_hotels(search_args: HotelsSearchArgs = Depends()):
+    return search_args
 
 
 class SBooking(BaseModel):
