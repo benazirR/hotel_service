14139ea220b286ea309db5c894d92f1cb0d915b1
B_R <b.rajabov@eskhata.tj>
2024-11-13 11:47:37 +0500

    create get token from cookies and use it for get bookings to each user individually

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,11 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, Depends
 
 from app.bookings.dao import BookingDAO
-from app.bookings.schemas import SBooking
+from app.users.dependencies import get_current_user
+from app.users.models import Users
 
 router = APIRouter(prefix="/bookings",
                    tags=["Bookings"],)
 
 @router.get("")
-async def get_bookings() -> list[SBooking]:
-    return await BookingDAO.find_all()
+async def get_bookings(user: Users = Depends(get_current_user)): # -> list[SBooking]:
+    return await BookingDAO.find_all(user_id=user.id)
