f4cc2ec62ba57302e53de3b72b34d4635cad3b8a
B_R <b.rajabov@eskhata.tj>
2024-11-13 08:35:05 +0500

    Fix the name of class

--- app/users/models.py
+++ app/users/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String
 from app.database import Base
 
-class User(Base):
+class Users(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, nullable=False)
