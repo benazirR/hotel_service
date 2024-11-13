9784ac22575cf0baf43274ea2ae31934f375dd61
B_R <b.rajabov@eskhata.tj>
2024-11-13 14:59:32 +0500

    Add logout method and about info

--- app/users/dependencies.py
+++ app/users/dependencies.py
@@ -5,6 +5,7 @@ from jose import jwt, JWTError
 
 from app.config import settings
 from app.users.dao import UsersDAO
+from app.users.models import Users
 
 
 def get_token(request: Request):
@@ -34,3 +35,8 @@ async def get_current_user(token: str = Depends(get_token)):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
 
     return user
+
+async def get_current_admin_user(current_user: Users = Depends(get_current_user)):
+    # if current_user.role != "admin":
+    #     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    return current_user
