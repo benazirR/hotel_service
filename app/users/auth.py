ffaf08fc9e24d22627255bdea204fa27d06bc4f3
B_R <b.rajabov@eskhata.tj>
2024-11-13 10:12:12 +0500

    create access_token function and authenticate_user async method

--- app/users/auth.py
+++ app/users/auth.py
@@ -1,4 +1,10 @@
+from datetime import datetime, timedelta
+from fastapi import HTTPException
+from jose import jwt
 from passlib.context import CryptContext
+from pydantic import EmailStr
+
+from app.users.dao import UsersDAO
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -7,3 +13,20 @@ def get_password_hash(password: str) -> str:
 
 def verify_password(plain_password, hashed_password) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=30)
+    to_encode.update({"exp": expire})
+    encode_jwt = jwt.encode(
+        to_encode, "sdfsdf", "HS256"
+    )
+    return encode_jwt
+
+
+async def authenticate_user(email: EmailStr, password: str):
+    user = await UsersDAO.find_one_or_none(email=email)
+    if not user and not verify_password(password, user.password):
+        return None
+    return user
