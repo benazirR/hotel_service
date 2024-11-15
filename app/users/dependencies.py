21fcb850a5e1226420493f8f0c3252bf190aa35f
B_R <b.rajabov@eskhata.tj>
2024-11-15 11:03:55 +0500

    clean some unnecessary methods

--- app/users/dependencies.py
+++ app/users/dependencies.py
@@ -12,7 +12,7 @@ from app.users.models import Users
 def get_token(request: Request):
     token = request.cookies.get("booking_access_token")
     if not token:
-        raise TokenAbsentException()
+        raise TokenAbsentException
     return token
 
 async def get_current_user(token: str = Depends(get_token)):
@@ -21,19 +21,19 @@ async def get_current_user(token: str = Depends(get_token)):
             token, settings.SECRET_KEY, settings.ALGORITHM
         )
     except JWTError:
-        raise IncorrectTokenFormat()
+        raise IncorrectTokenFormat
 
     expire: str = payload.get("exp")
     if (not expire) or (int(expire) < datetime.utcnow().timestamp()):
-        raise TokenExpiredException()
+        raise TokenExpiredException
 
     user_id: str = payload.get("sub")
     if not user_id:
-        raise UserIsNotPresentException()
+        raise UserIsNotPresentException
 
     user = await UsersDAO.find_by_id(int(user_id))
     if not user:
-        raise UserIsNotPresentException()
+        raise UserIsNotPresentException
 
     return user
 
