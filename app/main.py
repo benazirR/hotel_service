7d2b8216ed7f0ff25322572535cf205fca57fb68
B_R <b.rajabov@eskhata.tj>
2024-11-15 11:03:36 +0500

    clean some unnecessary methods

--- app/main.py
+++ app/main.py
@@ -1,13 +1,6 @@
-from dataclasses import Field
-
 from fastapi import FastAPI, Query, Depends
-from typing import Optional
-from datetime import date
-from pydantic import BaseModel
 
 from app.bookings.router import router as router_bookings
-from app.bookings.schemas import SBooking
-
 from app.users.router import router as router_users
 
 app = FastAPI()
@@ -15,18 +8,3 @@ app = FastAPI()
 # Подключаем роутеры
 app.include_router(router_users)
 app.include_router(router_bookings)
-
-class HotelsSearchArgs:
-    def __init__(self,
-                 location: str,
-                 date_from: date,
-                 date_to: date,
-                 stars: Optional[int] = Query(None, ge=1, le=5),
-                 has_spa: Optional[bool] = None
-                 ):
-        self.location = location
-        self.date_from = date_from
-        self.date_to = date_to
-        self.stars = stars
-        self.has_spa = has_spa
-
