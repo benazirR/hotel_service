1f7124cdd34b969ebf1fbd96968d1c001a6aa0a2
B_R <b.rajabov@eskhata.tj>
2024-11-15 10:23:57 +0500

    create add booking endpoint with sqlalchemy calculation about available rooms in hotels

--- app/bookings/dao.py
+++ app/bookings/dao.py
@@ -1,9 +1,42 @@
-from sqlalchemy import select
+from datetime import date
+
+from sqlalchemy import select, and_, or_, func
 
 from app.bookings.models import Bookings
 from app.dao.base import BaseDAO
-from app.database import async_session_maker
+from app.database import async_session_maker, engine
+from app.hotels.models import Rooms
 
 
 class BookingDAO(BaseDAO):
     model = Bookings
+
+    @classmethod
+    async def add(
+            cls,
+            user_id: int,
+            room_id: int,
+            date_from: date,
+            date_to: date,
+    ):
+        async with async_session_maker() as session:
+            booked_rooms = select(Bookings).filter(
+                and_(
+                Bookings.room_id == room_id,
+                    or_(and_(Bookings.date_from >= date_from, Bookings.date_from <= date_to),
+                        and_(Bookings.date_from <= date_from, Bookings.date_from > date_to))
+                )
+            ).cte("booked_rooms")
+
+            rooms_left = select(
+                (Rooms.quantity - func.count(booked_rooms.c.room_id)).label("rooms_left")
+            ).select_from(Rooms).join(
+                booked_rooms, booked_rooms.c.room_id == Rooms.id
+            ).where(Rooms.id == room_id).group_by(
+                Rooms.quantity, booked_rooms.c.room_id
+            )
+
+            print(rooms_left.compile(engine, compile_kwargs={"literal_binds": True}))
+
+            rooms_left = await session.execute(rooms_left)
+            print(rooms_left.scalar())
--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from fastapi import APIRouter, Request, Depends
 
 from app.bookings.dao import BookingDAO
@@ -10,3 +12,8 @@ router = APIRouter(prefix="/bookings",
 @router.get("")
 async def get_bookings(user: Users = Depends(get_current_user)): # -> list[SBooking]:
     return await BookingDAO.find_all(user_id=user.id)
+
+@router.post("")
+async def add_booking(room_id: int, date_from: date, date_to: date,
+                      user: Users = Depends(get_current_user)):
+    await BookingDAO.add(user.id, room_id, date_from, date_to)
