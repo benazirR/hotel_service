5f54c9113d052c775e1d1e2b141057281cd34578
B_R <b.rajabov@eskhata.tj>
2024-11-15 13:57:55 +0500

    create router for hotels, create schemas for hotels, create hotelsDAO

--- app/hotels/models.py
+++ app/hotels/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, String, Integer, ForeignKey, JSON
+from sqlalchemy.orm import relationship
 
 from app.database import Base
 
@@ -12,15 +13,7 @@ class Hotels(Base):
     rooms_quantity = Column(Integer, nullable=False)
     image_id = Column(Integer)
 
+    rooms = relationship("Rooms", back_populates="hotel")
 
-class Rooms(Base):
-    __tablename__ = 'rooms'
-
-    id = Column(Integer, primary_key=True, nullable=False)
-    hotel_id = Column(Integer, ForeignKey('hotels.id'), nullable=False)
-    name = Column(String,nullable=False)
-    description = Column(String,nullable=True)
-    price = Column(Integer,nullable=False)
-    services = Column(JSON, nullable=True)
-    quantity = Column(Integer,nullable=False)
-    image_id = Column(Integer)
+    def __str__(self):
+        return f"Hotel #{self.name} {self.location[:30]}"
---
+++ app/hotels/router.py
@@ -0,0 +1,23 @@
+from datetime import date, datetime, timedelta
+from typing import List
+
+from fastapi import APIRouter, Query
+
+from app.exceptions import DateFromCannotBeAfterDateTo, CannotBookHotelForLongPeriod
+from app.hotels.dao import HotelDAO
+from app.hotels.schemas import SHotelInfo
+
+router = APIRouter(prefix="/hotels", tags=["hotels"])
+
+@router.get("/{location}")
+async def get_hotels_by_location_and_time(
+        location: str,
+        date_from: date = Query(..., description=f"For example, {datetime.now().date()}"),
+        date_to: date = Query(..., description=f"For example, {(datetime.now() + timedelta(days=14)).date()}")
+) -> List[SHotelInfo]:
+    if date_from > date_to:
+        raise DateFromCannotBeAfterDateTo
+    if (date_to - date_from).days > 31:
+        raise CannotBookHotelForLongPeriod
+    hotels = await HotelDAO.find_all(location, date_from, date_to)
+    return hotels
